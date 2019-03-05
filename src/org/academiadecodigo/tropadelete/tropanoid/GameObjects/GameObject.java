799d3b9e1e4f157ed3993107f53d090b09f286e1
vascorodrigues <codecadet@smith.local>
2019-03-05 14:55:11 -0100

    final

--- src/org/academiadecodigo/tropadelete/tropanoid/GameObjects/GameObject.java
+++ src/org/academiadecodigo/tropadelete/tropanoid/GameObjects/GameObject.java
@@ -1,15 +1,11 @@
 package org.academiadecodigo.tropadelete.tropanoid.GameObjects;
 
-import org.academiadecodigo.simplegraphics.graphics.Color;
-
 public abstract class GameObject {
 
     protected double x;
     protected double y;
     protected int imageWidth;
     protected int imageHeight;
-
-    protected Color color;
 
     public int getImageHeight() {
         return imageHeight;
@@ -27,11 +23,4 @@ public abstract class GameObject {
         return y;
     }
 
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
 }
